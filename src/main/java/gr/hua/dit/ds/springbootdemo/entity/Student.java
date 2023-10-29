a04ab50a4d434832fed89830b71899f92657ddcf
Anargyros Tsadimas <tsadimas@hua.gr>
2023-10-29 18:46:01 +0200

    student entity and views

--- src/main/java/gr/hua/dit/ds/springbootdemo/entity/Student.java
+++ src/main/java/gr/hua/dit/ds/springbootdemo/entity/Student.java
@@ -1,23 +1,16 @@
 package gr.hua.dit.ds.springbootdemo.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
-@Entity
+
 public class Student {
 
-    @jakarta.persistence.Id
-    @Column
+
     private Integer Id;
 
-    @Column
     private String firstName;
-    @Column
     private String lastName;
 
-    @Column
     private String email;
 
     public Student(String firstName, String lastName, String email) {
